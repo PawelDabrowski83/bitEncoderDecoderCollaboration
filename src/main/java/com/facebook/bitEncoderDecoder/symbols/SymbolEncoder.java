f47d5d4b3f9bd4e806a7f0db03edade799c7b45f
Paweł Dąbrowski <fluorix@gmail.com>
2021-01-01 14:56:12 +0100

    delegated input validation to another method

--- src/main/java/com/facebook/bitEncoderDecoder/symbols/SymbolEncoder.java
+++ src/main/java/com/facebook/bitEncoderDecoder/symbols/SymbolEncoder.java
@@ -9,18 +9,24 @@ public class SymbolEncoder implements Encoder {
 
     @Override
     public String encode(String input) {
+        input = validateInput(input);
+
         StringBuilder encodeString = new StringBuilder();
-        if (input == null) {
-            throw new InputIsNullException();
-        } else if (input.equalsIgnoreCase(" ")) {
-            return "";
-        }
         for (int i = 0; i < input.length(); i++) {
             String multipliedChar = String.valueOf(input.charAt(i)).repeat(MULTIPLICATION_FACTOR);
             encodeString.append(multipliedChar);
-
         }
         return encodeString.toString();
+    }
+
+    private String validateInput(String input) {
+        if (input == null) {
+            throw new InputIsNullException();
+        }
+        if (input.isBlank()) {
+            return "";
+        }
+        return input;
     }
 
 }
